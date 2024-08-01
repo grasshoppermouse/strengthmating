18569469944d9c58388a01f0518d05cf6f00e744
Ed Hagen <ehagen@gmail.com>
2024-08-01 16:01:05 -0700

    Removed use of furrr. Cleaned up code

--- simulate.R
+++ simulate.R
@@ -1,13 +1,26 @@
 library(boot)
-library(furrr)
 
 set.seed(765456)
-
-plan("multisession")
 
 d_adult <- d_G[d_G$age >= 18 & d_G$age <= 60,]
 
 rqpois <- function(n, mu, theta) rnbinom(n=n, mu=mu, size=mu/(theta-1))
+
+param_dict <- c(
+  "(Intercept)" = "(Intercept)",
+  "sexfemale" = "sexfemale",
+  "age_centered" = "age_centered",
+  "partneredTRUE" = "partneredTRUE",
+  "strength_sex_centered" = "strength_sex_centered",
+  "sex_partners_scaled" = "sex_partners_scaled",
+
+  "sexfemale:strength_sex_centered" = "SEX_STRENGTH",
+  "strength_sex_centered:sexfemale" = "SEX_STRENGTH",
+  "age_centered:sexfemale" = "SEX_AGE",
+  "sexfemale:age_centered" = "SEX_AGE",
+  "strength_sex_centered:partneredTRUE" = "STRENGTH_PARTNERED",
+  "partneredTRUE:strength_sex_centered" = "STRENGTH_PARTNERED"
+)
 
 simdata <- function(
     N,
@@ -17,15 +30,12 @@ simdata <- function(
     partneredTRUE = 0,
     strength_sex_centered = 0,
     sex_partners_scaled = 0,
-    `sexfemale:strength_sex_centered` = 0,
-    `strength_sex_centered:sexfemale` = 0,
-    `age_centered:sexfemale` = 0,
-    `strength_sex_centered:partneredTRUE` = 0,
+    SEX_STRENGTH = 0,
+    SEX_AGE = 0,
+    STRENGTH_PARTNERED = 0,
     theta = 0,
     ...
 ){
-
-  SEXSTRENGTH <- ifelse(length(`sexfemale:strength_sex_centered`) > 0, `sexfemale:strength_sex_centered`, `strength_sex_centered:sexfemale`)
 
   tibble(
 
@@ -44,8 +54,8 @@ simdata <- function(
         sexfemale*sex +
         age_centered*agecentered +
         strength_sex_centered*strengthcentered +
-        SEXSTRENGTH*sex*strengthcentered +
-        `age_centered:sexfemale`*sex*agecentered
+        SEX_STRENGTH*sex*strengthcentered +
+        SEX_AGE*sex*agecentered
     )
     ),
     sexpartnersyear = rqpois(N, exp(
@@ -54,9 +64,9 @@ simdata <- function(
         age_centered*agecentered +
         partneredTRUE*partnered +
         strength_sex_centered*strengthcentered +
-        SEXSTRENGTH*sex*strengthcentered +
-        `age_centered:sexfemale`*sex*agecentered +
-        `strength_sex_centered:partneredTRUE`*partnered*strengthcentered),
+        SEX_STRENGTH*sex*strengthcentered +
+        SEX_AGE*sex*agecentered +
+        STRENGTH_PARTNERED*partnered*strengthcentered),
       theta
     ),
     sexpartners = rqpois(N, exp(
@@ -64,8 +74,9 @@ simdata <- function(
         sexfemale*sex +
         log(years_sexually_mature) +
         strength_sex_centered*strengthcentered +
-        SEXSTRENGTH*sex*strengthcentered +
-        `age_centered:sexfemale`*agecentered*sex
+        SEX_STRENGTH*sex*strengthcentered +
+        SEX_AGE*agecentered*sex +
+        partneredTRUE*partnered
     ),
     theta
     )
@@ -83,7 +94,7 @@ getstats_year <- function(params){
 #for lifetime partner models
 getstats_life <- function(params){
   d <- do.call(simdata, params)
-  m <- glm(sexpartners ~ sex*strengthcentered + offset(log(years_sexually_mature)), family = quasipoisson, d)
+  m <- glm(sexpartners ~ sex*strengthcentered + partnered + offset(log(years_sexually_mature)), family = quasipoisson, d)
   m_sum <- summary(m)
   m_sum$coefficients[, 4]
 }
@@ -98,11 +109,12 @@ getstats_partnered <- function(params){
 
 pwr <- function(N, model, getstatsfunction, outcome, scale_effect = 1){
   params <- as.list(coef(model))
+  names(params) <- param_dict[names(params)]
   params$N <- N
   params$strength_sex_centered <- scale_effect * params$strength_sex_centered
-  params$`sexfemale:strength_sex_centered` <- scale_effect * params$`sexfemale:strength_sex_centered`
+  params$SEX_STRENGTH <- scale_effect * params$SEX_STRENGTH
   params$theta <- summary(model)$dispersion[1]
-  pvalues <- future_map_dfr(1:1000, ~getstatsfunction(params), .options = furrr_options(seed = T))
+  pvalues <- map_dfr(1:1000, ~getstatsfunction(params))
   data.frame(
     strength = sum(pvalues$strengthcentered < 0.05)/1000,
     `sex X strength` = sum(pvalues$`sex:strengthcentered` < 0.05)/1000,
@@ -111,14 +123,15 @@ pwr <- function(N, model, getstatsfunction, outcome, scale_effect = 1){
 }
 
 # Sample size
-N <- sum(d_H$sex_partners <= 100, na.rm = T)
+N <- sum(d_H$sex_partners < 100, na.rm = T)
+scale_fctrs <- seq(0.25, 1, 0.25)
 
-pwr_partnered <- map(seq(0.25, 1, 0.25), \(x) pwr(N, models$Model$manth4, getstats_partnered, 'Partnered', scale_effect = x)) |> list_rbind()
-pwr_lifetime <- map(seq(0.25, 1, 0.25), \(x) pwr(N, models$Model$manth1, getstats_life, 'Lifetime partners', scale_effect = x)) |> list_rbind()
-pwr_pastyear <- map(seq(0.25, 1, 0.25), \(x) pwr(N, models$Model$manth2, getstats_year, 'Last year partners', scale_effect = x)) |> list_rbind()
+pwr_partnered <- map(scale_fctrs, \(x) pwr(N, models$Model$manth4, getstats_partnered, 'Partnered', scale_effect = x)) |> list_rbind()
+pwr_lifetime <- map(scale_fctrs, \(x) pwr(N, models$Model$manth1, getstats_life, 'Lifetime partners', scale_effect = x)) |> list_rbind()
+pwr_pastyear <- map(scale_fctrs, \(x) pwr(N, models$Model$manth2, getstats_year, 'Last year partners', scale_effect = x)) |> list_rbind()
 
 df_pwr <- bind_cols(
-  Scale = seq(0.25, 1, 0.25),
+  Scale = scale_fctrs,
   pwr_partnered,
   pwr_lifetime,
   pwr_pastyear,
